789f6bb7958a5ba4581b6b5fbefb0daaae1f0e87
Jeff Smith <jsmith3@vonage.com>
2019-06-25 16:27:30 -0400

    init

---
+++ lib/matchete.ex
@@ -0,0 +1,18 @@
+defmodule Matchete do
+  @moduledoc """
+  Documentation for Matchete.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Matchete.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/matchete_test.exs
@@ -0,0 +1,8 @@
+defmodule MatcheteTest do
+  use ExUnit.Case
+  doctest Matchete
+
+  test "greets the world" do
+    assert Matchete.hello() == :world
+  end
+end
